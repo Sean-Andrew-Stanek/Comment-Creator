1e92a6ce29fd8880f91c2a661c2ee723f61eda9f
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2024-05-12 17:54:22 +0900

    Gets English Comment, translates, returns

---
+++ config.py
@@ -0,0 +1,34 @@
+###################
+# Assistant Roles #
+###################
+
+teacher_assistant = {'role': 'assistant', 'content': 'A kind teacher who uses easy to understand words'}
+korean_translation = {'role': 'assistant', 'content': 'A translator for a family friend'}
+
+
+###################
+# System Roles #
+###################
+
+##############
+# User Roles #
+##############
+
+request_comment = {'role': 'user', 'content': 'Write a 3-5 sentence summary of academic and behavior of the ESL student for their report cards.'}
+request_translation = {'role': 'user', 'content': 'Please translate this into Korean'}
+
+
+##################
+# Base Variables #
+##################
+
+model = 'gpt-3.5-turbo'
+messages = [
+    teacher_assistant,
+    request_comment,
+]
+
+translation_messages = [
+    korean_translation,
+    request_translation
+]
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify, render_template, session;
+from flask import Flask, request, jsonify, render_template, make_response;
+import json
 import os
 import config
 from openai import OpenAI
@@ -34,3 +35,57 @@ app = Flask(__name__, static_folder='static')
 @app.route('/')
 def home():
     return render_template('index.html')
+
+### Chat Route
+@app.route('/comment', methods=['POST'])
+def makeComment():
+
+    try:
+        data = request.get_json()
+
+        # Response required to meet format    
+        if 'role' not in data or 'content' not in data:
+            error_response = {
+                'status': 'error',
+                'message': 'Missing required fields: role and content must be provided.'
+            }
+            return make_response(jsonify(error_response), 400)
+        
+        messages = config.messages
+        messages.append(data)
+
+        response = client.chat.completions.create(
+            model = config.model,
+            messages=messages
+        )
+        english_response = response.choices[-1].message.content
+        print(english_response);
+
+        translation_messages = config.translation_messages
+        translation_messages.append({'role': 'user', 'content': english_response})
+
+
+        translation = client.chat.completions.create(
+            model=config.model,
+            messages=translation_messages
+        )
+
+        return_message = jsonify({'English': english_response, 'Korean': translation.choices[-1].message.content})
+        print(return_message)
+        return make_response(return_message, 200)
+    
+    except json.JSONDecodeError:
+        error_response = {
+            'status': 'error',
+            'message': 'Invalid request.'
+        }
+        return make_response(jsonify(error_response), 400)
+        
+if __name__ == '__main__':
+    app.run(debug=True)
+
+            
+    
+
+
+
